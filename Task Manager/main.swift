daa5f030d4ce7262eafce06efea7b4acaf5d4b71
Jim Williams <jimwilliams@macbook-pro-7.local>
2018-09-12 10:08:55 -0500

    Initial Commit

---
+++ Task Manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task Manager
+//
+//  Created by Jim Williams on 9/12/18.
+//  Copyright © 2018 Visioncast Solutions. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
